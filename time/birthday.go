88264697ba0b1251919ac2269c8f2041af66c40a
Ayakura Yuki <YukiAyakura@outlook.com>
2025-04-28 20:39:27 +0800

    @ AgeNumber now accepts superset

--- time/birthday.go
+++ time/birthday.go
@@ -8,8 +8,8 @@ import (
 )
 
 type AgeNumber interface {
-	int | int8 | int16 | int32 | int64 |
-		uint | uint8 | uint16 | uint32 | uint64
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
 // CalculateRealAge returns the real age from birthday to now.
